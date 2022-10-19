8e5ae6920f61584491b3faed905bf4eabe0f6126
Jake Sadowski <110630130+jsadowski7@users.noreply.github.com>
2022-10-19 18:51:48 -0500

    correct API call with key

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,1 @@
-//var requestUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat={30.2672}&lon={97.7431}&appid={5e4289338a39885f2081a3f5d3603510}';
+var requestUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat=30.2672&lon=97.7431&appid=655f440e2edd3fdcfbfbcd81a9465bc3';

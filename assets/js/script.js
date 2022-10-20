0b017b1d0c1bfa8bf4eb740f33935d4ceaba08c8
Jake Sadowski <110630130+jsadowski7@users.noreply.github.com>
2022-10-20 17:06:23 -0500

    Added forecast containers and current day info container to html file, linked jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-var requestUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat=30.2672&lon=97.7431&appid=655f440e2edd3fdcfbfbcd81a9465bc3';
+// 5-Day Forecast API call //
+// Added Lat and Lon for ATX, Added imperial units //
+var requestUrl = 'http://api.openweathermap.org/data/2.5/forecast?units=imperial&lat=30.2672&lon=97.7431&appid=655f440e2edd3fdcfbfbcd81a9465bc3';
+
+fetch('http://api.openweathermap.org/data/2.5/forecast?Austin,us&units=imperial&lat=30.2672&lon=-97.7431&appid=655f440e2edd3fdcfbfbcd81a9465bc3', {
+})
+.then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });

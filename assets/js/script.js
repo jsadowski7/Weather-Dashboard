5943b51e3daf53cb05261afb6a223026754e596c
Jake Sadowski <110630130+jsadowski7@users.noreply.github.com>
2022-10-23 15:45:22 -0500

    Html refactoring for search bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,36 @@
 // 5-Day Forecast API call //
 // Added Lat and Lon for ATX, Added imperial units //
-var requestUrl = 'http://api.openweathermap.org/data/2.5/forecast?units=imperial&lat=30.2672&lon=97.7431&appid=655f440e2edd3fdcfbfbcd81a9465bc3';
+//var requestUrl = 'http://api.openweathermap.org/data/2.5/forecast?units=imperial&lat=30.2672&lon=97.7431&appid=655f440e2edd3fdcfbfbcd81a9465bc3';
+var city = "";
+var searchCity = $("#search-city");
+var searchButton = $("#search-button");
+var clearButton = $("#clear-history");
+var currentCity = $("#current-city");
+var APIKey = "655f440e2edd3fdcfbfbcd81a9465bc3";
+var units = "units=imperial&";
+var lat = "lat=30.2672&";
+var lon = "lon=-97.7431&";
+var queryURL = "http://api.openweathermap.org/data/2.5/forecast?" + units + lat + lon + "APPID=" + APIKey;
 
-fetch('http://api.openweathermap.org/data/2.5/forecast?Austin,us&units=imperial&lat=30.2672&lon=-97.7431&appid=655f440e2edd3fdcfbfbcd81a9465bc3', {
+console.log(queryURL);
+
+fetch(queryURL, {
 })
 .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    console.log(data);
-  });
+   console.log(data);
+ });
+
+
+
+
+function displayWeather(event) {
+    event.preventDefault();
+    if (searchCity.val().trim()!=="") {
+        city = searchCity.val().trim();
+        currentWeather(city);
+    }
+  }
+
